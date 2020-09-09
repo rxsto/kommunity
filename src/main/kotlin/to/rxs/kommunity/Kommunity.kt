bd58ae53bb0657b43c92f30875d8f84dc21ea36c
Oskar Lang <oskar.risto.lang@gmail.com>
2020-09-09 20:45:43 +0200

    feat: add callbackserver

--- src/main/kotlin/to/rxs/kommunity/Kommunity.kt
+++ src/main/kotlin/to/rxs/kommunity/Kommunity.kt
@@ -1,4 +1,4 @@
-package to.rxs
+package to.rxs.kommunity
 
 import com.gitlab.kordlib.common.entity.Status
 import com.gitlab.kordlib.core.Kord
@@ -9,14 +9,21 @@ import com.gitlab.kordlib.kordx.commands.kord.model.prefix.kord
 import com.gitlab.kordlib.kordx.commands.kord.model.prefix.mention
 import com.gitlab.kordlib.kordx.commands.model.prefix.literal
 import com.gitlab.kordlib.kordx.commands.model.prefix.or
-import com.gitlab.kordlib.kordx.commands.model.prefix.prefix
+import io.ktor.client.request.*
 import kapt.kotlin.generated.configure
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import mu.KotlinLogging
-import to.rxs.core.GameAnimator
-import to.rxs.io.connect
+import to.rxs.kommunity.core.GameAnimator
+import to.rxs.kommunity.io.connect
+import to.rxs.kommunity.listeners.registerJoinRolesListener
+import to.rxs.kommunity.util.httpClient
+import to.rxs.kommunity.youtube.CallbackServer
+import to.rxs.kommunity.youtube.YouTubeEventSubscriber
+import to.rxs.kommunity.youtube.YoutubeEventListener
+import java.awt.Color
+import java.time.Instant
 
 private val log = KotlinLogging.logger {}
 
@@ -53,9 +60,15 @@ class Kommunity {
         connect()
 
         client.on<ReadyEvent> {
-            log.info { "Starting game animator ...." }
+            log.info { "Starting game animator..." }
             gameAnimator.start()
         }
+
+        log.info { "Initializing member join listener..." }
+        client.registerJoinRolesListener()
+
+        CallbackServer.registerListener(YoutubeEventListener(client))
+        CallbackServer.start()
     }
 
     private fun shutdown() {
